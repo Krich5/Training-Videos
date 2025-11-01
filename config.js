640fff5b260925830ad6ac5d5ddd5677d2939812
Krich5 <165947471+Krich5@users.noreply.github.com>
2025-11-01 15:44:18 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Shared configuration for all Training Portal pages
 window.APP_CONFIG = {
   // Update this URL when you redeploy your Google Apps Script
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzGrB7zRFtzR9YtGSoTrPKH-OcP6g5hCBFWs6PX226ounFsEgEANTvRSk6E6nxEi7Q/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzANcSixCAEfvqcj7fOfAQn7TaHkeYcOh3msNSeuwkK2eKwdLTqgQ0U9jCJXSyrswJW/exec",
   
   // Must match the SECRET_TOKEN in your Apps Script
   ADMIN_TOKEN: "CXASTrainingPortal25",
