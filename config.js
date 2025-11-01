887edd9ad8315cdad66dff0089a5063938240602
Krich5 <165947471+Krich5@users.noreply.github.com>
2025-11-01 15:22:21 -0400

    Change SCRIPT_URL in config.js
    Updated SCRIPT_URL for Google Apps Script deployment.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Shared configuration for all Training Portal pages
 window.APP_CONFIG = {
   // Update this URL when you redeploy your Google Apps Script
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyje304aYZy14yfyQrE28_bqkmdBqZKKvFCZZMpmx7YEhWOtkSdAYtPEQoStV24DRw/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyxMlLT08KltIAE_Fqka8JACX7fF9jLt8PMg4SfPdcIUZhhZD_NG69DIF-dvprN-iOx/exec",
   
   // Must match the SECRET_TOKEN in your Apps Script
   ADMIN_TOKEN: "CXASTrainingPortal25",
